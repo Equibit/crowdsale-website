d106cd5397954b1178691f47510f6c085d9328dd
Marc Godard <me@marcgodard.com>
2017-09-04 15:43:22 -0400

    Progress

--- easy-app/src/pages/page-home/page-home.js
+++ easy-app/src/pages/page-home/page-home.js
@@ -2,8 +2,6 @@ import Component from 'can-component'
 import DefineMap from 'can-define/map/map'
 import './page-home.less'
 import view from './page-home.stache'
-import $ from 'jquery'
-import 'fullpage.js'
 
 export const ViewModel = DefineMap.extend({
   message: {
@@ -14,16 +12,5 @@ export const ViewModel = DefineMap.extend({
 export default Component.extend({
   tag: 'page-home',
   ViewModel,
-  view,
-  events: {
-    inserted() {
-      console.log("inserted")
-      $('#fullpage').fullpage()
-			$.fn.fullpage.reBuild();
-    },
-    remove() {
-      console.log("removed")
-			$.fn.fullpage.destroy('all');
-    }
-  }
+  view
 })
