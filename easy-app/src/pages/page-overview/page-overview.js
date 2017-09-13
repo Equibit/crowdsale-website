6c35e51895583751ac1f33f1425d6d39cdb597a4
Marc Godard <me@marcgodard.com>
2017-09-13 17:01:45 -0400

    More progress

--- easy-app/src/pages/page-overview/page-overview.js
+++ easy-app/src/pages/page-overview/page-overview.js
@@ -6,6 +6,13 @@ import view from './page-overview.stache';
 export const ViewModel = DefineMap.extend({
   message: {
     value: 'This is the page-overview component'
+  },
+  question: {
+    type: 'number',
+    value: 1
+	},
+  changeQ(num) {
+    this.question = num
   }
 });
 
