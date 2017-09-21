eba245c95ecd9fa96553627cd1f57f5c8ec1f17a
Marc Godard <me@marcgodard.com>
2017-09-20 22:03:44 -0400

    Search tickets

--- src/pages/admin-tickets/admin-tickets.js
+++ src/pages/admin-tickets/admin-tickets.js
@@ -5,6 +5,7 @@ import view from './admin-tickets.stache'
 import Ticket from '~/models/admin-ticket'
 
 export const ViewModel = DefineMap.extend({
+	search: 'string',
 	appState: {
 		type: 'any'
 	},
@@ -19,12 +20,17 @@ export const ViewModel = DefineMap.extend({
 			skip: 'number',
 			limit: 'number',
 			total: 'number'
-		}))({skip: 0, limit: 30})
+		}))({skip: 0, limit: 50})
 	},
-	loadPage () {
+	loadPage (params) {
 		this.loadingTickets = true
 		let pagination = this.pagination
-		Ticket.getList({$skip: pagination.skip, $limit: pagination.limit})
+		const query = {$skip: pagination.skip, $limit: pagination.limit}
+		params = Object.assign({search: this.search}, params)
+		if (params.search) {
+			query['$search'] = params.search
+		}
+		Ticket.getList(query)
 			.then(tickets => {
 				this.rows = tickets
 				this.pagination.total = tickets.total
@@ -34,6 +40,11 @@ export const ViewModel = DefineMap.extend({
 				if (err.status === 401) this.appState.error401()
 				else console.log(err)
 			})
+	},
+	doSearch() {
+		if (!this.loadingTickets) {
+			this.loadPage({search: this.search})
+		}
 	}
 })
 
--- src/pages/admin-tickets/ticket-details/ticket-details.js
+++ src/pages/admin-tickets/ticket-details/ticket-details.js
@@ -2,16 +2,36 @@ import Component from 'can-component'
 import DefineMap from 'can-define/map/map'
 import './ticket-details.less'
 import view from './ticket-details.stache'
+import Ticket from '~/models/admin-ticket'
 
 export const ViewModel = DefineMap.extend({
 	appState: {
 		type: 'any'
 	},
-  linkSubject: 'string'
+  linkSubject: 'string',
+	loadingTicket: {
+		value: true
+	},
+	ticketHistory: {
+		type: 'any'
+	}
 })
 
 export default Component.extend({
   tag: 'ticket-details',
   ViewModel,
-  view
+  view,
+	events: {
+		inserted: function(){
+			Ticket.get(this.viewModel.linkSubject)
+				.then(ticket => {
+					console.log(ticket)
+					setTimeout(() => this.viewModel.loadingTicket = false, 25)
+				})
+				.catch(err => {
+					if (err.status === 401) this.appState.error401()
+					else console.log(err)
+				})
+		}
+	}
 })
