98695b51e969b143ca936e39857fdb85f496d8d1
Marc Godard <me@marcgodard.com>
2017-09-20 20:15:30 -0400

    Text Update

--- src/pages/page-about/page-about.js
+++ src/pages/page-about/page-about.js
@@ -12,5 +12,24 @@ export const ViewModel = DefineMap.extend({
 export default Component.extend({
   tag: 'page-about',
   ViewModel,
-  view
+  view,
+	events: {
+		inserted: function() {
+			$('a').click(function() {
+				let target = $(this.hash)
+				if (target.length) {
+					if ($(this).hasClass('ignore-href')) return true
+					if ($('.navbar-toggle').is(':visible') && $('.navbar-collapse').hasClass('in')) $('.navbar-toggle').trigger('click')
+					$('html, body').stop()
+					$('html,body').animate({
+						scrollTop: target.offset().top - 90
+					}, 1000)
+					return false
+				}
+			})
+		},
+		remove: function() {
+			$('a').off('click')
+		}
+	}
 });
