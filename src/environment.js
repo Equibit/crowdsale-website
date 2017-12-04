c847c57d3e587f3636d5dc3c88fa7481757f8acc
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-12-04 00:40:50 -0500

    fixed api url

--- src/environment.js
+++ src/environment.js
@@ -1,6 +1,6 @@
 let hostname = window.location.hostname
 let proto = window.location.protocol
-let host = hostname === 'localhost' ? 'localhost:3030' : `api-${hostname}`
+let host = hostname === 'localhost' ? 'localhost:3030' : `${hostname}`.replace('eqb.', 'api.')
 
 export default {
   apiUrl: `${proto}//${host}`,
