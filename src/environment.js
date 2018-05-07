565ab95096588521c64f04ab5d8506ce8b19e296
Ilya Fadeev <fadeev.ilya@gmail.com>
2018-05-07 14:53:24 -0400

    updated api url for heroku

--- src/environment.js
+++ src/environment.js
@@ -1,6 +1,11 @@
 let hostname = window.location.hostname
 let proto = window.location.protocol
-let host = hostname === 'localhost' ? 'localhost:3030' : `${hostname}`.replace('eqb.', 'api.')
+// let host = hostname === 'localhost' ? 'localhost:3030' : `${hostname}`.replace('eqb.', 'api.')
+let host = hostname === 'localhost' ? 'localhost:3030' : `${hostname}`.replace('website', 'api')
+
+// Heroku DNS:
+// https://qa-crowdsale-website.herokuapp.com/
+// https://qa-crowdsale-api.herokuapp.com/
 
 export default {
   apiUrl: `${proto}//${host}`,
