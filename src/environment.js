dafcc5bbf6d5def87807ef1262d50ee67a41777a
Ilya Fadeev <fadeev.ilya@gmail.com>
2018-05-25 02:06:40 -0400

    fixed environment api url for heroku

--- src/environment.js
+++ src/environment.js
@@ -1,6 +1,7 @@
 let hostname = window.location.hostname
 let proto = window.location.protocol
 let host = hostname === 'localhost' ? 'localhost:3030' : `${hostname}`.replace('eqb.', 'api.')
+  .replace('website', 'api')
 
 export default {
   apiUrl: `${proto}//${host}`,
