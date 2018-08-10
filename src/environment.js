69d4380cfece5cb3da162d3556dd69e8923141df
KvagaJack <kvagajack@gmail.com>
2018-08-10 11:39:19 -0400

    fixed env api link

--- src/environment.js
+++ src/environment.js
@@ -1,7 +1,11 @@
 let hostname = window.location.hostname
 let proto = window.location.protocol
-let host = hostname === 'localhost' ? 'localhost:3030' : `${hostname}`.replace('eqb.', 'api.')
-  .replace('website', 'api')
+let host = hostname === 'localhost'
+  ? 'localhost:3030'
+  : `${hostname}`
+    .replace('eqb.', 'api.')
+    .replace('eqb-', 'api-')
+    .replace('website', 'api')
 
 export default {
   apiUrl: `${proto}//${host}`,
